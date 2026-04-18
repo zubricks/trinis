ab9171d3d4eccea764d116b2f414e46149edbea7
Sean Zubrickas <zubricks@gmail.com>
2026-04-18 18:39:48 -0400

    adds blob storage

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,3 +1,4 @@
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { formBuilderPlugin } from '@payloadcms/plugin-form-builder'
 import { nestedDocsPlugin } from '@payloadcms/plugin-nested-docs'
 import { redirectsPlugin } from '@payloadcms/plugin-redirects'
@@ -24,6 +25,12 @@ const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
 }
 
 export const plugins: Plugin[] = [
+  vercelBlobStorage({
+    collections: {
+      media: true,
+    },
+    token: process.env.BLOB_READ_WRITE_TOKEN || '',
+  }),
   redirectsPlugin({
     collections: ['pages', 'posts'],
     overrides: {
